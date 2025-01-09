116f2cec61f128fd796c03f2df51683b9f2b3eb8
Nouran Elshazly <nouranelshazly19@gmail.com>
2025-01-09 15:18:21 +0200

    updatesomefiles

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,13 @@
-import { getCart } from './productLogic.js';
-
-var imgarr = [
-  '../photos/1.png',
-  '../photos/2.png',
-  '../photos/3.png',
-  '../photos/4.png',
-  '../photos/1.png',
-];
-var i = 0;
-function startshow() {
-  setInterval(nextfun, 1000);
-}
-function nextfun() {
-  i++;
-  if (i >= imgarr.length) i = 0;
-  myimg.src = imgarr[i];
-}
+var imgarr= ["../photos/1.png","../photos/2.png","../photos/3.png","../photos/4.png","../photos/1.png"];
+        var i=0;
+          function startshow(){
+              t=setInterval("nextfun()",1000);
+          }
+          function nextfun(){
+              i++;
+              if(i>=imgarr.length) i=0;
+              myimg.src= imgarr[i];
+          }
 
           function toggleMenu() {
             const nav = document.querySelector('.nav');
@@ -29,15 +21,10 @@ function nextfun() {
             $(this).next('.dropdown-menu').toggleClass('show');
         });
     
-  // Close the dropdown when clicking outside
-  $(document).on('click', function () {
-      $('#dropdown-menu').removeClass('show');
+        // Close the dropdown when clicking outside
+        $(document).on('click', function () {
+            $('.dropdown-menu').removeClass('show');
+        });
+        
     });
-
-    $('#menu-toggle').on('click', function () {
-      toggleMenu();
-    });
-
-  let cart = getCart();
-  $('#cart-count').text(cart.length);
-});
+ 
