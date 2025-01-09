b86a138cab3805d7b1df3d0040f0c6edfe6b8ae2
Eng/Ibrahim Shahin <ibrahimsaaed38@gmail.com>
2025-01-09 08:24:27 +0200

    move home logic to script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,44 @@
-import { getCart } from "./productLogic.js";
+import { getCart } from './productLogic.js';
 
-var imgarr= ["../photos/1.png","../photos/2.png","../photos/3.png","../photos/4.png","../photos/1.png"];
-        var i=0;
-          function startshow(){
-              setInterval(nextfun,1000);
-          }
-          function nextfun(){
-              i++;
-              if(i>=imgarr.length) i=0;
-              myimg.src= imgarr[i];
-          }
+var imgarr = [
+  '../photos/1.png',
+  '../photos/2.png',
+  '../photos/3.png',
+  '../photos/4.png',
+  '../photos/1.png',
+];
+var i = 0;
+function startshow() {
+  setInterval(nextfun, 1000);
+}
+function nextfun() {
+  i++;
+  if (i >= imgarr.length) i = 0;
+  myimg.src = imgarr[i];
+}
 
-          function toggleMenu() {
-            const nav = document.querySelector('.nav');
-            const menuToggle = document.getElementById('menu-toggle');
-            nav.classList.toggle('menu-show');
-        }
-        $(function(){
-        startshow()
-        $('#dropdown-toggle').on('click', function (e) {
-            e.stopPropagation();
+function toggleMenu() {
+  const nav = document.querySelector('.nav');
+  const menuToggle = document.getElementById('menu-toggle');
+  nav.classList.toggle('menu-show');
+}
+$(function () {
+  startshow();
+  $('.dropdown-toggle').on('click', function (e) {
+    // e.stopPropagation();
+
+    $('#dropdown-menu').toggleClass('show');
+  });
     
-            $(this).next('.dropdown-menu').toggleClass('show');
-        });
-    
-        // Close the dropdown when clicking outside
-        $(document).on('click', function () {
-            $('.dropdown-menu').removeClass('show');
-        });
-        let cart = getCart()
-        $('#cart-count').text(cart.length)
-        
+  // Close the dropdown when clicking outside
+  $(document).on('click', function () {
+      $('#dropdown-menu').removeClass('show');
     });
 
- 
+    $('#menu-toggle').on('click', function () {
+      toggleMenu();
+    });
+
+  let cart = getCart();
+  $('#cart-count').text(cart.length);
+});
